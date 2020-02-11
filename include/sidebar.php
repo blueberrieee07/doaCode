518e6289e2f34fa8588c5d6b1a759ff6586c0c14
blueberrieee07 <omekung477@gmail.com>
2020-02-11 09:54:29 +0700

    v11

--- include/sidebar.php
+++ include/sidebar.php
@@ -1,37 +1,114 @@
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+ <!-- <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar"> -->
 
-  <!-- Sidebar - Brand -->
-  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
-    <div class="sidebar-brand-icon rotate-n-15">
-      <i class="fas fa-laugh-wink"></i>
-    </div>
-    <div class="sidebar-brand-text mx-3">SB Admin <sup></sup></div>
-  </a>
+ <ul class="navbar-nav bg-gradient-blue sidebar sidebar-dark accordion" id="accordionSidebar">
 
-  <!-- Divider -->
-  <hr class="sidebar-divider my-0">
+   <!-- Sidebar - Brand -->
+   <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+     <div class="sidebar-brand-icon rotate-n-15">
+       <i class="fas fa-laugh-wink"></i>
+     </div>
+     <div class="Den of Art text mx-3">Den of Art <sup>12</sup></div>
+   </a>
 
-  <!-- Nav Item - Dashboard -->
-  <li class="nav-item">
-    <a class="nav-link" href="calendar.php">
-      <i class="fas fa-fw fa-calendar"></i>
-      <span>Calendar</span></a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="add.php">
-      <i class="fas fa-fw fa-user"></i>
-      <span>add</span></a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="login.php">
-      <i class="fas fa-fw fa-user"></i>
-      <span>login</span></a>
-  </li>
+   <!-- Divider -->
+   <hr class="sidebar-divider my-0">
 
-  <!-- Divider -->
-  <hr class="sidebar-divider">
+   <!-- Nav Item - กลับไปหน้าหลัก -->
+   <li class="nav-item active">
+     <a class="nav-link" href="index.html">
+       <i class="fas fa-fw fa-tachometer-alt"></i>
+       <span>หน้าหลัก</span></a>
+   </li>
 
-  <!-- Heading -->
+   <!-- Divider -->
+   <hr class="sidebar-divider">
+
+   <!-- Heading -->
+   <div class="sidebar-heading">
+
+   </div>
+
+   <!-- Nav Item - Pages Collapse Menu -->
+   <li class="nav-item">
+     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+       <i class="fas fa-fw fa-cog"></i>
+       <span>ตั้งค่า</span>
+     </a>
+     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+       <div class="bg-white py-2 collapse-inner rounded">
+         <h6 class="collapse-header">Custom Components:</h6>
+         <a class="collapse-item" href="buttons.html">Buttons</a>
+       </div>
+     </div>
+   </li>
 
 
-</ul>
+
+   <!-- Divider -->
+   <hr class="sidebar-divider">
+
+   <!-- Heading -->
+   <div class="sidebar-heading">
+     เมนู
+   </div>
+
+   <!-- Nav Item - Pages Collapse Menu -->
+   <li class="nav-item">
+     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+       <i class="fas fa-fw fa-folder"></i>
+       <span>บริการทางทันตกรรม</span>
+     </a>
+     <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+       <div class="bg-white py-2 collapse-inner rounded">
+         <h6 class="collapse-header">ข้อมูลการรักษา:</h6>
+         <a class="collapse-item" href="login.html">ทันตกรรมจัดฟัน</a>
+         <a class="collapse-item" href="register.html">ทันตกรรมอุดฟัน</a>
+         <a class="collapse-item" href="forgot-password.html">ถอนฟัน</a>
+         <a class="collapse-item" href="forgot-password.html">ฟันปลอม</a>
+         <a class="collapse-item" href="404.html">รักษารากฟัน</a>
+         <a class="collapse-item" href="blank.html">ทันตกรรมเพื่อความสวยงาม</a>
+       </div>
+     </div>
+   </li>
+
+   <!-- Nav Item - Charts -->
+   <li class="nav-item">
+     <a class="nav-link" href="charts.html">
+       <i class="fas fa-fw fa-chart-area"></i>
+       <span>ราคา&โปรโมชั่น</span></a>
+   </li>
+
+   <!-- Nav Item - Tables -->
+   <li class="nav-item">
+     <a class="nav-link" href="tables.html">
+       <i class="fas fa-fw fa-table"></i>
+       <span>ปฏิทินการรักษา</span></a>
+   </li>
+   <!-- Nav Item - Tables -->
+   <li class="nav-item">
+     <a class="nav-link" href="tables.html">
+       <i class="fas fa-fw fa-table"></i>
+       <span>เกี่ยวกับเรา</span></a>
+   </li>
+   <!-- Nav Item - Tables -->
+   <li class="nav-item">
+     <a class="nav-link" href="tables.html">
+       <i class="fas fa-fw fa-table"></i>
+       <span>ทันตแพทย์</span></a>
+   </li>
+   <!-- Nav Item - Tables -->
+   <li class="nav-item">
+     <a class="nav-link" href="tables.html">
+       <i class="fas fa-fw fa-table"></i>
+       <span>ค่าบริการ</span></a>
+   </li>
+
+   <!-- Divider -->
+   <hr class="sidebar-divider d-none d-md-block">
+
+   <!-- Sidebar Toggler (Sidebar) -->
+   <div class="text-center d-none d-md-inline">
+     <button class="rounded-circle border-0" id="sidebarToggle"></button>
+   </div>
+
+ </ul>
