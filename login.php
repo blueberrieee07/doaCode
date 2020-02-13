b7e150fc8784d6d1c2eb57d0cf29db69ff347345
blueberrieee07 <omekung477@gmail.com>
2020-02-13 11:47:39 +0700

    om

--- login.php
+++ login.php
@@ -27,75 +27,71 @@
 
 <body class="bg-gradient-primary">
 
-  
-    <div class="container">
 
-      <!-- Outer Row -->
-      <div class="row justify-content-center">
+  <div class="container">
 
-        <div class="col-xl-10 col-lg-12 col-md-9">
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
 
-          <div class="card o-hidden border-0 shadow-lg my-5">
-            <div class="card-body p-0">
-              <!-- Nested Row within Card Body -->
-              <div class="row">
-                <div class="col-lg-7 d-none d-lg-block bg-login-image">
-                  <img src="img/logo.jpg" alt="">
-                </div>
-                <div class="col-lg-5">
-                  <div class="p-5">
-                    <div class="text-center">
-                      <h1 class="h4 text-gray-900 mb-4">Welcome</h1>
-                    </div>
-                    <form class="user">
-                      <div class="form-group">
-                        <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
-                      </div>
-                      <div class="form-group">
-                        <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
-                      </div>
-                      <div class="form-group">
-                        <div class="custom-control custom-checkbox small">
-                          <input type="checkbox" class="custom-control-input" id="customCheck">
-                          <label class="custom-control-label" for="customCheck">Remember Me</label>
-                        </div>
-                      </div>
-<<<<<<< HEAD
-                      <a href="index.php" class="btn btn-primary btn-user btn-block">
-=======
-                      <a href="index.html" class="btn btn-primary btn-user btn-block">
->>>>>>> 01c011bf4d9c06df30d29ff800d8ff2e6c66d9de
-                        Login
-                      </a>
+      <div class="col-xl-10 col-lg-12 col-md-9">
 
-                      <hr>
-                      <div class="text-center">
-                        <a class="small" href="forgot-password.html">Forgot Password?</a>
-                      </div>
-                      <div class="text-center">
-                        <a class="small" href="register.html">Create an Account!</a>
-                      </div>
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+              <div class="col-lg-7 d-none d-lg-block bg-login-image">
+                <img src="img/logo.jpg" alt="">
+              </div>
+              <div class="col-lg-5">
+                <div class="p-5">
+                  <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-4">Welcome</h1>
                   </div>
+                  <form class="user">
+                    <div class="form-group">
+                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                    </div>
+                    <div class="form-group">
+                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                      <div class="custom-control custom-checkbox small">
+                        <input type="checkbox" class="custom-control-input" id="customCheck">
+                        <label class="custom-control-label" for="customCheck">Remember Me</label>
+                      </div>
+                    </div>
+                    <a href="index.php" class="btn btn-primary btn-user btn-block">
+                      Login
+                    </a>
+
+                    <hr>
+                    <div class="text-center">
+                      <a class="small" href="forgot-password.html">Forgot Password?</a>
+                    </div>
+                    <div class="text-center">
+                      <a class="small" href="register.html">Create an Account!</a>
+                    </div>
                 </div>
               </div>
             </div>
           </div>
-
         </div>
 
       </div>
 
     </div>
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  </div>
 
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
 
 </body>
 
