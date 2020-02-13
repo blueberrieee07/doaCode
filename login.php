cb41b565d9b6febfb0719e5b9d943ecb4c6475c7
blueberrieee07 <omekung477@gmail.com>
2020-02-13 11:49:43 +0700

    ome

--- login.php
+++ login.php
@@ -45,7 +45,7 @@
               <div class="col-lg-5">
                 <div class="p-5">
                   <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Welcome</h1>
+                    <h1 class="h4 text-gray-900 mb-4">Welcomesdv d</h1>
                   </div>
                   <form class="user">
                     <div class="form-group">
