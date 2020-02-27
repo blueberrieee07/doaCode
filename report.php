d2aee143594eb1d8a28743765cb662d164264bcf
pornphipat phungput <titoslownow0000@gmail.com>
2020-02-27 17:33:43 +0700

    hg

--- report.php
+++ report.php
@@ -10,5 +10,35 @@
 <body>
       <h1 class="text-center">ค่าใช่จ่ายในการรักษา</h1> 
      <div>
-         <input type="text">form
+     <h1 > ชื่อ <input type="text"id="fname" name="fname"> </h1>
+     <h1 > ที่อยุ่ <input type="text"id="address" name="address"> </h1>
+     <h1 > เบอร์โทร <input type="text"id="tel" name="tel"> </h1>
+     <h1 > วัน เดือน ปี <input type="text"id="date" name="date"> </h1>
+     <h1 > เพศ <input type="text"id="sex" name="sex"> </h1>
      </div>
+     <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+  </table>
