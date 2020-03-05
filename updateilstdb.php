5efe1d3d6f75663f5214d0d3f409411c5455d4d7
blueberrieee07 <omekung477@gmail.com>
2020-03-05 10:56:27 +0700

    11111

--- updateilstdb.php
+++ updateilstdb.php
@@ -1,8 +1,8 @@
 <meta charset="utf-8">
 <?php
-// echo '<pre>';
-// print_r($_POST);
-// echo '</pre>';
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
 // exit;
 $firstname = $_POST['firstname'];
 $lastname = $_POST['lastname'];
@@ -12,19 +12,20 @@ $age = $_POST['age'];
 $idcard = $_POST['idcard'];
 $date = $_POST['date'];
 $address = $_POST['address'];
-$number = $_POST['number'];
 $position = $_POST['position'];
 $university = $_POST['university'];
 $branch = $_POST['branch'];
 $graduationyear = $_POST['graduationyear'];
 $specifically = $_POST['specifically'];
-$emai = $_POST['email'];
+$phone = $_POST['number'];
+$email = $_POST['email'];
+
 
 
 include('include/condb.php');
 
 
-$check = "SELECT username FROM users WHERE username = '$user'";
+$check = "SELECT username FROM users  WHERE username = '$email'";
 $result1 = mysqli_query($condb, $check) or die(mysqli_error());
 $num = mysqli_num_rows($result1);
 if ($num > 0) {
@@ -35,19 +36,16 @@ if ($num > 0) {
     echo "</script>";
     exit;
 } else {
-    $sql = "INSERT INTO `users` 
+    $sql = "INSERT INTO users
     (
             `username`,
             `password`, 
             `firstname`, 
             `lastname`,
-            `sax`, 
-            `email`, 
-            `registerdate`,
-            `status`,
+            `email`,
+            `sax`,
             `idcard`,
             `address`,
-            `number`,
             `age`, 
             `phone`,
             `day`,
@@ -55,32 +53,27 @@ if ($num > 0) {
             `university`,
             `branch`, 
             `graduationyear`, 
-            `specifically`, 
-            `date`
+            `specifically`
             ) 
                VALUES 
                (
-               [value-2],
-               [value-3],
-               [value-4],
-               [value-5],
-               [value-6],
-               [value-7],
-               [value-8],
-               [value-9],
-               [value-10],
-               [value-11],
-               [value-12],
-               [value-13],
-               [value-14],
-               [value-15],
-               [value-16],
-               [value-17],
-               [value-18],
-               [value-19],
-               [value-20],
-               [value-21]
-               
+                '$email',
+                '$password',
+                '$firstname',
+                '$lastname',
+                '$email',
+                '$sax',
+                '$idcard',
+                '$address',
+                '$age', 
+                '$phone',
+                '$date',
+                '$position',
+                '$university',
+                '$branch', 
+                '$graduationyear', 
+                '$specifically'
+
                )";
     $result = mysqli_query($condb, $sql) or die("Error in query: $sql" . mysqli_error());
 }
@@ -90,11 +83,11 @@ mysqli_close($condb);
 if ($result) {
     echo "<script type='text/javascript'>";
     echo "alert('เพิ่มช้อมูลสำเร็จ');";
-    echo "window.location='add.php';";
+    echo "window.location='ilstUser.php';";
     echo "</script>";
 } else {
     echo "<script type='text/javascript'>";
     // echo "alert('Error!');";
-    echo "window.location='add.php';";
+    echo "window.location='ilstUser.php';";
     echo "</script>";
 }
